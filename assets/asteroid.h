511558daaa890acaa342790ad6edf39bd55476ed
Leftium <john@leftium.com>
2019-05-05 16:05:02 +0900

    Add multimedia assets to repo.
    These files are combined into the ASTEROID.dat file with the
    Allegro DAT tool.

---
+++ assets/asteroid.h
@@ -0,0 +1,20 @@
+/* Allegro datafile object indexes, produced by grabber v4.2.3, MSVC */
+/* Datafile: c:\Db\Doc\Asteroid\dat\ASTEROID.DAT */
+/* Date: Fri Mar 12 08:07:50 2010 */
+/* Do not hand edit! */
+
+#define _Ammo1                           0        /* BMP  */
+#define _Ammo2                           1        /* BMP  */
+#define _Bar1                            2        /* BMP  */
+#define _Bar2                            3        /* BMP  */
+#define _Boom                            4        /* SAMP */
+#define _Engine                          5        /* SAMP */
+#define _Explode                         6        /* BMP  */
+#define _Game_palette                    7        /* PAL  */
+#define _music                           8        /* MIDI */
+#define _Rock                            9        /* BMP  */
+#define _Ship1                           10       /* BMP  */
+#define _Ship2                           11       /* BMP  */
+#define _Shoot                           12       /* SAMP */
+#define _COUNT                           13
+
